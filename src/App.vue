485f2d79e4ef10216da8fde2a5c3bcdeb2e9051c
romain-garcia-rodriguez <romain.garcia@hotmail.com>
2023-01-05 20:16:26 +0100

    Init project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <!-- 1) Afficher votre nom complet (prénom + nom dans 2 variables différentes) en utilsant une computed  -->
+  <!-- 2) Afficher votre age multiplié par 7 -->
+  <!-- 3) Afficher un nombre aléatoire entre 0 et 1 -->
+  <!-- 4) Prendre une image sur Google et l'afficher de manière dynamique avec Vue -->
+  <!-- 5) Ajouter une valeur par défaut (stocké dans 1 variable) dans un input via Vue en utilisant les attribute bindings -->
+  <div></div>
+</template>
+
+<style></style>
